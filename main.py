35ff539bb19efe9576fdc27c7461178439216fab
Amirabbas Fazelinia <utilyre@gmail.com>
2025-02-24 10:34:54 +0200

    implement beautiful console output

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from stats import count_words, count_each_char, sort_num_chars
+from stats import count_words, count_individual_chars, prettify_char_counts
 
 def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
+    filename = "books/frankenstein.txt"
 
-    num_words = count_words(frankenstein)
-    print(f"{num_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at {filename}...")
+    content = read_file(filename)
+    num_words = count_words(content)
+    char_counts = prettify_char_counts(count_individual_chars(content))
 
-    num_chars = count_each_char(frankenstein)
-    sorted = sort_num_chars(num_chars)
-    for char, count in sorted:
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+    for char, count in char_counts:
         print(f"{char}: {count}")
 
-def get_book_text(filename):
+def read_file(filename):
     with open(filename) as f:
         contents = f.read()
         return contents
--- stats.py
+++ stats.py
@@ -2,17 +2,17 @@ def count_words(string):
     words = string.split()
     return len(words)
 
-def count_each_char(string):
-    num_chars = {}
+def count_individual_chars(string):
+    char_counts = {}
     for char in string:
         char = char.lower()
-        if char in num_chars:
-            num_chars[char] += 1
+        if char in char_counts:
+            char_counts[char] += 1
         else:
-            num_chars[char] = 1
-    return num_chars
+            char_counts[char] = 1
+    return char_counts
 
-def sort_num_chars(num_chars):
+def prettify_char_counts(num_chars):
     list = [(char, count) for char, count in num_chars.items()]
     list.sort(key=lambda tup : tup[1], reverse=True)
     return list
