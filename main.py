cdc7f34e8e55321ec1b8276f99ab38735b0bb2c0
Amirabbas Fazelinia <utilyre@gmail.com>
2025-02-24 10:20:15 +0200

    count each char

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from stats import count_words
+from stats import count_words, count_each_char
 
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
+
     num_words = count_words(frankenstein)
     print(f"{num_words} words found in the document")
+
+    num_chars = count_each_char(frankenstein)
+    print(num_chars)
 
 def get_book_text(filename):
     with open(filename) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def count_words(string):
     words = string.split()
     return len(words)
+
+def count_each_char(string):
+    num_chars = {}
+    for char in string:
+        char = char.lower()
+        if char in num_chars:
+            num_chars[char] += 1
+        else:
+            num_chars[char] = 1
+    return num_chars
